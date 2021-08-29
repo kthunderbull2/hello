843b5f00147c8eb8792a746ba29a71a9fad24a2e
Kade ThunderBull <kthunderbull2@huskers.unl.edu>
2021-08-29 19:44:08 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Kade ThunderBull
+ * Date:   8/29/2021
+ *
+ * A simple program that prints name
+ */
+
+/* includes */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* main function */
+int main(int argC, char **argV)
+{
+    printf("Kade ThunderBull!\n");
+
+    return 0;
+}
